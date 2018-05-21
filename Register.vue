dc07296eb3528e30d32f6c2319aa47fe474d5e28
Alex Bull <abu59@uclive.ac.nz>
2018-05-22 01:00:30 +1200

    Register user functionality added

--- Register.vue
+++ Register.vue
@@ -1,15 +1,91 @@
 <template>
   <div id="register">
+
     <router-link :to="{ name: 'home'}">Home</router-link>
+
+    <br /><br />
+
+    <h1>Register User</h1>
+    Username:
+    <input type="text" v-model="username" placeholder="Enter username">
+    <br /><br />
+    Given Name:
+    <input type="text" v-model="givenName" placeholder="Enter given name">
+    <br /><br />
+    Family Name:
+    <input type="text" v-model="familyName" placeholder="Enter family name">
+    <br /><br />
+    Email:
+    <input type="email" v-model="email" placeholder="Enter email">
+    <br /><br />
+    Password:
+    <input type="password" v-model="password" placeholder="Enter password">
+    <br /><br />
+
+    <input type="submit" v-on:click="req">
+
+    <div v-if="errorFlag" style="color: red;">
+      <br />
+      Details provided entered incorrectly or not unique.
+      <br />
+      Please check input.
+    </div>
+
+    <div v-if="userData" style="color: red;" v>
+      <br />
+      {{ redirect() }}
+      Successfully registered. Redirecting home...
+    </div>
+
+
   </div>
 </template>
 
 <script>
-    export default {
-        name: "register"
+  export default {
+    data() {
+      return {
+        userData: "",
+        error: "",
+        errorFlag: false,
+        email: "",
+        username: "",
+        password: "",
+        givenName: "",
+        familyName: ""
+      }
+    },
+    mounted: function () {
+    },
+    methods: {
+      req: function () {
+        console.log(this.reqBody());
+        this.$http.post("http://localhost:4941/api/v1/users", this.reqBody(), "application/json")
+          .then(function (response) {
+            this.errorFlag = false;
+            this.userData = response.data;
+            console.log("success");
+          }, function (error) {
+            this.error = error;
+            this.errorFlag = true;
+          });
+      },
+      reqBody: function () {
+        return JSON.stringify({
+          username: this.username,
+          givenName: this.givenName,
+          familyName: this.familyName,
+          email: this.email,
+          password: this.password
+        })
+      },
+      redirect: function () {
+        let thisInstance = this;
+        setTimeout(function() {
+          thisInstance.$router.push({name: 'home'})
+        }, 2000);
+      }
     }
+  }
+
 </script>
-
-<style scoped>
-
-</style>
